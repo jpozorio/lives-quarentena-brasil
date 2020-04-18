4185e65fa7fd225c866d2ec58e8eede0305e8fcb
João Paulo Ozório da Silva <joaopaulo@zgsolucoes.com.br>
2020-04-17 21:03:05 -0300

    ler arquivo json para não precisar fazer deploy

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,10 +1,20 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-import {products} from '../products';
+import * as data_json from '../products.json';
 
 interface Style {
     value: string;
     viewValue: string;
+}
+
+interface Product {
+    name: string,
+    time: string,
+    date: string,
+    description: string,
+    link: string,
+    estilo: string,
+    iniciado: boolean,
 }
 
 @Component({
@@ -12,7 +22,7 @@ interface Style {
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     styles: Style[] = [
         {value: '', viewValue: 'Todos'},
         {value: 'sertanejo', viewValue: 'Sertanejo'},
@@ -22,17 +32,24 @@ export class ProductListComponent {
     ];
     selected = '';
 
-    products = products.filter(p => {
-        return p.date >= new Date();
-    });
+    products;
 
-    share() {
-        window.alert('The product has been shared!');
+    ngOnInit(): void {
+        let employee: Product[] = (data_json as any).default;
+        let agora = new Date();
+        this.products = employee.filter(p => {
+            let date = new Date(p.date);
+            date.setHours(date.getHours() + 3);
+            return date >= agora;
+        });
+        this.products.forEach(p => {
+            let date = new Date(p.date);
+            p.iniciado = agora > date;
+            if (date.getMinutes() == 0) {
+                p.time = date.getHours() + "h";
+            } else {
+                p.time = date.getHours() + ":" + date.getMinutes() + "h";
+            }
+        })
     }
 }
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
